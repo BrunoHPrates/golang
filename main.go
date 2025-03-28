e13dbd9a6ad24aed5af8592732e64726dfb899bc
BrunoHenrique <bruno.h.silva32@aluno.senai.br>
2025-03-28 10:35:36 -0300

    digite dois números para calcular

--- main.go
+++ main.go
@@ -6,8 +6,11 @@ func main(){
 
 	var a float32
 	var b float32
-		fmt.Println("A soma é: ", a+b)
-		fmt.Println("A subtração é: ", a-b)
-		fmt.Println("A multiplicação é: ", a*b)
-		fmt.Println("A divisão é: ", a/b)
+	fmt.Println("DIgite dois números: ", a, b)
+	fmt.Scan(&a)
+	fmt.Scan(&b)
+	fmt.Println("A soma desses 2 números é: ", a + b)
+	fmt.Println("A subtração desses 2 números é: ", a - b)
+	fmt.Println("A multiplicação desses 2 números é: ", a * b)
+	fmt.Println("A divisão desses 2 números é: ", a / b)
 }
