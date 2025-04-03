6a28c51ab732cc51b7295f349c2f61381f8decf6
BrunoHenrique <bruno.h.silva32@aluno.senai.br>
2025-04-03 09:19:48 -0300

    Vetor2

--- main.go
+++ main.go
@@ -9,4 +9,20 @@ func main(){
 	fmt.Println(nomes)
 	nomes[3] = "Miles Morales"
 	fmt.Println(nomes)
+	// Slice
+	var score = []int{100, 200, 300, 400}
+	fmt.Println(score)
+	score[1] = 2
+	fmt.Println(score, len(score), cap(score))
+	rangeOne := score[1:3]
+	fmt.Println(rangeOne)
+	rangetwo := score[2:]
+	fmt.Println(rangetwo)
+	rangeThree := score[:3]
+	fmt.Println(rangeThree)
+
+	var superherois = []string{"Deadpool", "Motoqueiro Fantasma"}
+	fmt.Println(superherois)
+	superherois = append(superherois, "Ben 10")
+	fmt.Println(superherois, len(superherois), cap(superherois))
 }
