d618e52b319ae62cc693008d4643386e06fdc9c0
BrunoHenrique <bruno.h.silva32@aluno.senai.br>
2025-04-04 09:41:13 -0300

    FOR

--- main.go
+++ main.go
@@ -20,4 +20,31 @@ func main(){
 	sort.Strings(names)
 	fmt.Println(names)
 	fmt.Println(sort.SearchStrings(names, "Yan"))
+	x := 0
+
+	for x < 5 {
+		fmt.Println(x)
+		x++
+	}
+
+	for i := 0; i < 5; i++ {
+		fmt.Println("for 2", i)
+	}
+
+	for i:= 0; i <len(names); i++ {
+		fmt.Println(names[1])
+	}
+
+	for index, value := range names{
+		fmt.Println("O index é", index, "e o valor é", value)
+	}
+
+	for indice, valor := range ages {
+		fmt.Println("O ", indice, "é ", valor)
+	}
+
+	superherois := []string{"Homem Aranha", "Pantera Negra", "Homem de Ferro"}
+	for index, value := range superherois {
+		fmt.Println("O número do herói:", index, "e o nome do herói: ", value)
+	}
 }
