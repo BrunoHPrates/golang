792d09c1fc31b70bab09cfa803cdb4767eecb613
BrunoHenrique <bruno.h.silva32@aluno.senai.br>
2025-04-04 08:23:30 -0300

    Biblioteca string

--- main.go
+++ main.go
@@ -1,28 +1,23 @@
 package main
 
-import "fmt"
+import ("fmt" 
+		"strings"
+		"sort")
 
 func main(){
-	var ages = [4]int{15, 16, 16, 16}
-	nomes := [4]string{"Yan", "Eduardo", "Pedro", "Vini"}
+	greeting := "Hello my friends!"
+	fmt.Println(strings.Contains(greeting, "friends"))
+	fmt.Println(strings.ReplaceAll(greeting, "Hello", "Hi"))
+	fmt.Println(strings.ToUpper(greeting))
+	fmt.Println(strings.Index(greeting,"my"))
+	fmt.Println(strings.Split(greeting, " "))
+	ages := []int {50, 80, 10}
+	sort.Ints(ages)
 	fmt.Println(ages)
-	fmt.Println(nomes)
-	nomes[3] = "Miles Morales"
-	fmt.Println(nomes)
-	// Slice
-	var score = []int{100, 200, 300, 400}
-	fmt.Println(score)
-	score[1] = 2
-	fmt.Println(score, len(score), cap(score))
-	rangeOne := score[1:3]
-	fmt.Println(rangeOne)
-	rangetwo := score[2:]
-	fmt.Println(rangetwo)
-	rangeThree := score[:3]
-	fmt.Println(rangeThree)
-
-	var superherois = []string{"Deadpool", "Homem -aranha", "Motoqueiro Fantasma"}
-	fmt.Println(superherois)
-	superherois = append(superherois, "Ben 10", "Mulher Maravilha")
-	fmt.Println(superherois, len(superherois), cap(superherois))
+	index := sort.SearchInts(ages, 50)
+	fmt.Println(index)
+	names := []string{"Bruno", "Yan", "Eduardo"}
+	sort.Strings(names)
+	fmt.Println(names)
+	fmt.Println(sort.SearchStrings(names, "Yan"))
 }
