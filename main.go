c96eb69660b2fc1d3c3d8f888dc893e27776a4c7
BrunoHenrique <bruno.h.silva32@aluno.senai.br>
2025-04-10 08:23:31 -0300

    vetor soma

--- main.go
+++ main.go
@@ -1,50 +1,19 @@
 package main
 
-import ("fmt" 
-		"strings"
-		"sort")
+import ("fmt")
 
 func main(){
-	greeting := "Hello my friends!"
-	fmt.Println(strings.Contains(greeting, "friends"))
-	fmt.Println(strings.ReplaceAll(greeting, "Hello", "Hi"))
-	fmt.Println(strings.ToUpper(greeting))
-	fmt.Println(strings.Index(greeting,"my"))
-	fmt.Println(strings.Split(greeting, " "))
-	ages := []int {50, 80, 10}
-	sort.Ints(ages)
-	fmt.Println(ages)
-	index := sort.SearchInts(ages, 50)
-	fmt.Println(index)
-	names := []string{"Bruno", "Yan", "Eduardo"}
-	sort.Strings(names)
-	fmt.Println(names)
-	fmt.Println(sort.SearchStrings(names, "Yan"))
-	x := 0
-
-	for x < 5 {
-		fmt.Println(x)
-		x++
-	}
-
-	for i := 0; i < 5; i++ {
-		fmt.Println("for 2", i)
-	}
-
-	for i:= 0; i <len(names); i++ {
-		fmt.Println(names[1])
-	}
-
-	for index, value := range names{
-		fmt.Println("O index é", index, "e o valor é", value)
-	}
-
-	for indice, valor := range ages {
-		fmt.Println("O ", indice, "é ", valor)
-	}
-
-	superherois := []string{"Homem Aranha", "Pantera Negra", "Homem de Ferro"}
-	for index, value := range superherois {
-		fmt.Println("O número do herói:", index, "e o nome do herói: ", value)
-	}
+	numeros:= [10]float32{}
+	fmt.Println("Digite 5 números números.")
+	fmt.Println("Digite o primeiro número:")
+	fmt.Scan(&numeros[0])
+	fmt.Println("Digite o segundo número:")
+	fmt.Scan(&numeros[1])
+	fmt.Println("Digite o terceiro número:")
+	fmt.Scan(&numeros[2])
+	fmt.Println("Digite o quarto número:")
+	fmt.Scan(&numeros[3])
+	fmt.Println("Digite o quinto número:")
+	fmt.Scan(&numeros[4])
+	fmt.Println("A soma dos números é", numeros[0]+numeros[1]+numeros[2]+numeros[3]+numeros[4])
 }
