c411eed96897219e788f25263577f71cb2da0824
BrunoHenrique <bruno.h.silva32@aluno.senai.br>
2025-04-10 11:04:17 -0300

    Saque e depósito

--- main.go
+++ main.go
@@ -5,24 +5,56 @@ import (
 )
 
 func main() {
-	var idade int
-    fmt.Println("Por favor digite sua idade:")
-    fmt.Scan(&idade)
-    if idade < 0 || idade > 120 {
-    for {
-        fmt.Println("Idade inválida.")
-        fmt.Println("Por favor digite uma idade válida:")
-        fmt.Scan(&idade)
-        if (idade > 0 && idade < 120) {
-            break
-        }
-    }
-    }
-    if idade < 18 {
-        fmt.Println("Você é menor de idade.")
-    }   else if idade >= 18 && idade < 60 {
-        fmt.Println("Você é maior de idade.")
-    } else {
-        fmt.Println("Você é idoso.")
-    }
+	var saldo float64
+	var resultado float64
+	for {
+	fmt.Print("Digite o saldo inicial: ")
+	fmt.Scan(&saldo)
+	if saldo < 0  {
+		fmt.Println("Valor inválido")
+	} else { 
+		break
+	}
+	} 
+	var opcao string
+	for {
+	fmt.Println("Você deseja sacar ou depositar? (s/d)")
+	fmt.Scan(&opcao)
+	if opcao != "s" && opcao != "d" {
+		fmt.Println("Opção inválida")
+	} else {
+		break
+	}
 }
+	if opcao == "s" {
+		var saque float64
+		for {
+		fmt.Println("Digite o valor do saque: ")
+		fmt.Scan(&saque)
+		if saque > saldo || saque <= 0 {
+			fmt.Println("Valor inválido")
+		} else {
+		resultado = saldo - saque
+		fmt.Println("Saque de ", saque," reais realizado com sucesso!")
+		fmt.Println("Seu saldo atual é de ", resultado, " reais")
+			break
+		}
+		}
+	}
+	if opcao == "d" {
+		var deposito float64
+		for {
+		fmt.Println("Digite o valor do deposito: ")
+		fmt.Scan(&deposito)
+		if deposito <= 0 {
+			fmt.Println("Valor inválido")
+		} else {
+			resultado = saldo + deposito
+			fmt.Println("Depósito de ", deposito," reais realizado com sucesso!")
+			fmt.Println("Seu saldo atual é de ", resultado, " reais")
+			break
+		}
+		}
+	}
+}
+	
