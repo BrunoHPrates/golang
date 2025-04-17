92dc9374299ef1dfa60f07868df3990602e6df15
BrunoHenrique <bruno.h.silva32@aluno.senai.br>
2025-04-17 10:59:52 -0300

    primeiro map

--- main.go
+++ main.go
@@ -4,23 +4,20 @@ import (
 	"fmt"
 )
 
-func dadosPessoa(nome string, idade int)(string, int) {
-	if idade < 0 {
-		return "Idade inválida", 0
-	} else if idade >= 0 && idade <= 18 {
-		return "Menor de idade", idade
-	} else  {
-		return nome, idade
-	}
-}
-
 func main() {
-	var nome string
-	var idade int
-	fmt.Println("Digite seu nome:")
-	fmt.Scanln(&nome)
-	fmt.Println("Digite sua idade:")
-	fmt.Scanln(&idade)
-	fmt.Println(dadosPessoa(nome, idade))
-
+	alunoIdade := make(map[string]int)
+	alunoIdade["Bruno"] = 15
+	alunoIdade["Yan"] = 16
+	alunoIdade["Fabiano"] = 40
+	alunoIdade["Isabela"] = 15
+	fmt.Println("Idade do Bruno:", alunoIdade["Bruno"])
+	notasAlunos := map[string]float64{
+		"Bruno":   9.7,
+		"Yan":     10,
+		"Fabiano": 8.7,
+		"Isabela": 9.5,
+	}
+	for nome,nota := range notasAlunos{
+		fmt.Printf("%s tirou nota %.2f", nome, nota)
+	}
 }
