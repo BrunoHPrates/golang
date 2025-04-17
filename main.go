b7b5c0f45b274a51db643434959e42fdf4b5ae30
BrunoHenrique <bruno.h.silva32@aluno.senai.br>
2025-04-17 08:30:55 -0300

    funções com mais de um retorno

--- main.go
+++ main.go
@@ -1,66 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func saldoInicial() float64 {
-	var saldo float64
-	for {
-		fmt.Println("Bem-Vindo (a)!")
-		fmt.Println("Digite seu saldo inicial:")
-		fmt.Scan(&saldo)
-		if saldo < 0 {
-			fmt.Println("Saldo inválido!")
-		} else {
-			break
-		}
+func dividir(dividendo int, divisor int) (int, string){
+	if divisor == 0 {
+		return 0, "Erro na divisão por 0"
 	}
-	return saldo
+	return dividendo / divisor, "sem erro"
 }
 
-func menu() int {
-	var opcao int
-	fmt.Println("\nEscolha uma opção:")
-	fmt.Println("1. Depositar")
-	fmt.Println("2. Sacar")
-	fmt.Println("3. Ver saldo")
-	fmt.Println("4. Sair")
-	fmt.Scan(&opcao)
-	return opcao
+func operacaoBasilca(a int, b int) (int, int, int) {
+	soma := a + b
+	multiplicacao := a * b	
+	subtracao := a - b
+	return soma, multiplicacao, subtracao
 }
 
 func main() {
-	saldo := saldoInicial()
+ resultado, erro := dividir(10, 2)
+	 if erro != "sem erro" {
+		fmt.Println(erro)
+	} else {
+		fmt.Println("O resultado da divisão é:", resultado)
+ }
 
-	for {
-		opcao := menu()
-		switch opcao {
-		case 1:
-			var deposito float64
-			fmt.Println("Digite quanto você deseja depositar:")
-			fmt.Scan(&deposito)
-			if deposito <= 0 {
-				fmt.Println("Valor inválido!")
-			} else {
-				saldo += deposito
-				fmt.Println("Depósito realizado. Seu saldo atual é:", saldo)
-			}
-		case 2:
-			var saque float64
-			fmt.Println("Digite quanto você deseja sacar:")
-			fmt.Scan(&saque)
-			if saque > saldo || saque <= 0 {
-				fmt.Println("Valor inválido!")
-			} else {
-				saldo -= saque
-				fmt.Println("Saque realizado. Seu saldo atual é:", saldo)
-			}
-		case 3:
-			fmt.Println("Seu saldo atual é:", saldo)
-		case 4:
-			fmt.Println("Saindo... Obrigado por usar o sistema!")
-			return
-		default:
-			fmt.Println("Opção inválida!")
-		}
-	}
+	soma, multi, sub := operacaoBasilca(10, 2) 
+ 	fmt.Println(soma)
+	fmt.Println(multi)
+	fmt.Println(sub)
+
+
 }
