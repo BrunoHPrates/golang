b36bf5a4ada1f7159435d90270ac5f00afbd2267
BrunoHenrique <bruno.h.silva32@aluno.senai.br>
2025-04-17 09:23:34 -0300

    função idade e nome

--- main.go
+++ main.go
@@ -4,32 +4,23 @@ import (
 	"fmt"
 )
 
-func dividir(dividendo int, divisor int) (int, string){
-	if divisor == 0 {
-		return 0, "Erro na divisão por 0"
+func dadosPessoa(nome string, idade int)(string, int) {
+	if idade < 0 {
+		return "Idade inválida", 0
+	} else if idade >= 0 && idade <= 18 {
+		return "Menor de idade", idade
+	} else  {
+		return nome, idade
 	}
-	return dividendo / divisor, "sem erro"
-}
-
-func operacaoBasilca(a int, b int) (int, int, int) {
-	soma := a + b
-	multiplicacao := a * b	
-	subtracao := a - b
-	return soma, multiplicacao, subtracao
 }
 
 func main() {
- resultado, erro := dividir(10, 2)
-	 if erro != "sem erro" {
-		fmt.Println(erro)
-	} else {
-		fmt.Println("O resultado da divisão é:", resultado)
- }
-
-	soma, multi, sub := operacaoBasilca(10, 2) 
- 	fmt.Println(soma)
-	fmt.Println(multi)
-	fmt.Println(sub)
-
+	var nome string
+	var idade int
+	fmt.Println("Digite seu nome:")
+	fmt.Scanln(&nome)
+	fmt.Println("Digite sua idade:")
+	fmt.Scanln(&idade)
+	fmt.Println(dadosPessoa(nome, idade))
 
 }
