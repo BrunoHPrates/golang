168f8eca88403ab443dfec11e4e708294ed5b4c1
BrunoHenrique <bruno.h.silva32@aluno.senai.br>
2025-04-24 13:26:35 -0300

    funcao Barry Allen

--- main.go
+++ main.go
@@ -1,23 +1,12 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-func main() {
-	alunoIdade := make(map[string]int)
-	alunoIdade["Bruno"] = 15
-	alunoIdade["Yan"] = 16
-	alunoIdade["Fabiano"] = 40
-	alunoIdade["Isabela"] = 15
-	fmt.Println("Idade do Bruno:", alunoIdade["Bruno"])
-	notasAlunos := map[string]float64{
-		"Bruno":   9.7,
-		"Yan":     10,
-		"Fabiano": 8.7,
-		"Isabela": 9.5,
-	}
-	for nome,nota := range notasAlunos{
-		fmt.Printf("%s tirou nota %.2f", nome, nota)
-	}
+func pegarNome() (string, string) {
+	return "Barry", "Allen"
+}
+
+func main (){
+	nome, sobrenome := pegarNome()
+	fmt.Println(nome, sobrenome)
 }
