5bf564baa82cac79eba3e273d17e938d64bdcaa9
BrunoHenrique <bruno.h.silva32@aluno.senai.br>
2025-04-24 14:55:40 -0300

    estoque cantina

--- main.go
+++ main.go
@@ -3,21 +3,22 @@ package main
 import "fmt"
 
 func main (){
-	capitais := map[string]string{
-		"SP": "São Paulo",
-		"RJ": "Rio de Janeiro",
-		"ES": "Espírito Santo",
+	estoque := make(map[string]int)
+
+	estoque["coxinha"] = 10
+	estoque["pâo de queijo"] = 15
+	estoque["refrigerante"] = 20	
+
+	for k,v := range estoque {
+	fmt.Println("Produto, Quantidade", k, v)
 	}
+	
+	estoque["coxinha"] -= 2
+	estoque["pâo de queijo"] -= 1
+	 
+	fmt.Printf("\nProdutos após a venda: \n")
+	for k,v := range estoque {
+		fmt.Println("Produto, Quantidade", k, v)
+		}
 
-	capitais["BH"] = "Belo Horizonte"
-
-	for k,v := range capitais {
-		fmt.Println("Sigla, Nome", k, v)
-	}
-
-	delete(capitais, "AC")
-
-	for k,v := range capitais {
-		fmt.Println("Sigla, Nome", k, v)
-	}
 }
